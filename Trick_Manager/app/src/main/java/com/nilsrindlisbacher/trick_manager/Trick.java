415a87648437f2c239704641a8183c992ffeff0d
Nils Rindlisbacher <mex69@gmx.ch>
2024-06-18 21:34:08 +0200

    Create Trick.java

---
+++ Trick_Manager/app/src/main/java/com/nilsrindlisbacher/trick_manager/Trick.java
@@ -0,0 +1,23 @@
+package com.nilsrindlisbacher.trick_manager;
+
+public class Trick {
+
+    String trick_name;
+    String trick_type;
+
+    public String getTrick_name() {
+        return trick_name;
+    }
+
+    public void setTrick_name(String trick_name) {
+        this.trick_name = trick_name;
+    }
+
+    public String getTrick_type() {
+        return trick_type;
+    }
+
+    public void setTrick_type(String trick_type) {
+        this.trick_type = trick_type;
+    }
+}
